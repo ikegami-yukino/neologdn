155f761d2599ac9f40a22953f4ca9533a49107d1
Yukino Ikegami <yknikgm@gmail.com>
2015-10-10 00:22:44 +0900

    Add benchmark

---
+++ benchmark/normalize_neologd.py
@@ -0,0 +1,54 @@
+# encoding: utf8
+"""
+from: https://github.com/neologd/mecab-ipadic-neologd/wiki/Regexp.ja
+"""
+from __future__ import unicode_literals
+import re
+import unicodedata
+
+def unicode_normalize(cls, s):
+    pt = re.compile('([{}]+)'.format(cls))
+
+    def norm(c):
+        return unicodedata.normalize('NFKC', c) if pt.match(c) else c
+
+    s = ''.join(norm(x) for x in re.split(pt, s))
+    return s
+
+def remove_extra_spaces(s):
+    s = re.sub('[ 　]+', ' ', s)
+    blocks = ''.join(('\u4E00-\u9FFF',  # CJK UNIFIED IDEOGRAPHS
+                      '\u3040-\u309F',  # HIRAGANA
+                      '\u30A0-\u30FF',  # KATAKANA
+                      '\u3000-\u303F',  # CJK SYMBOLS AND PUNCTUATION
+                      '\uFF00-\uFFEF'   # HALFWIDTH AND FULLWIDTH FORMS
+                      ))
+    basic_latin = '\u0000-\u007F'
+
+    def remove_space_between(cls1, cls2, s):
+        p = re.compile('([{}]) ([{}])'.format(cls1, cls2))
+        while p.search(s):
+            s = p.sub(r'\1\2', s)
+        return s
+
+    s = remove_space_between(blocks, blocks, s)
+    s = remove_space_between(blocks, basic_latin, s)
+    s = remove_space_between(basic_latin, blocks, s)
+    return s
+
+def normalize_neologd(s):
+    s = s.strip()
+    s = unicode_normalize('０−９Ａ-Ｚａ-ｚ｡-ﾟ', s)
+
+    def maketrans(f, t):
+        return {ord(x): ord(y) for x, y in zip(f, t)}
+
+    s = s.translate(
+        maketrans('!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~｡､･｢｣',
+                  '！”＃＄％＆’（）＊＋，−．／：；＜＝＞？＠［￥］＾＿｀｛｜｝〜。、・「」'))
+    s = re.sub('[˗֊‐‑‒–⁃⁻₋−]+', '-', s)  # normalize hyphens
+    s = re.sub('[﹣－ｰ—―─━ー]+', 'ー', s)  # normalize choonpus
+    s = re.sub('[~∼∾〜〰～]', '', s)  # remove tildes
+    s = remove_extra_spaces(s)
+    s = unicode_normalize('！”＃＄％＆’（）＊＋，−．／：；＜＞？＠［￥］＾＿｀｛｜｝〜', s)  # keep ＝,・,「,」
+    return s
