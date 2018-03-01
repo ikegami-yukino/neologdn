d151f629de4dd88d88cd962f475b7e34f216337b
Yukino Ikegami <yknikgm@gmail.com>
2018-03-02 01:44:59 +0900

    replace " -> ' and delete unicode literal u

--- test_neologdn.py
+++ test_neologdn.py
@@ -7,35 +7,35 @@ from neologdn import normalize
 class TestNeologdn(unittest.TestCase):
 
     def test_normalize(self):
-        self.assertEqual(normalize("０"), "0")
-        self.assertEqual(normalize("ﾊﾝｶｸ"), "ハンカク")
-        self.assertEqual(normalize("o₋o"), "o-o")
-        self.assertEqual(normalize("majika━"), "majikaー")
-        self.assertEqual(normalize("わ〰い"), "わい")
-        self.assertEqual(normalize("スーパーーーー"), "スーパー")
-        self.assertEqual(normalize("!#"), "!#")
-        self.assertEqual(normalize("ゼンカク　スペース"), "ゼンカクスペース")
-        self.assertEqual(normalize("お             お"), "おお")
-        self.assertEqual(normalize("      おお"), "おお")
-        self.assertEqual(normalize("おお      "), "おお")
-        self.assertEqual(normalize("検索 エンジン 自作 入門 を 買い ました!!!"),\
-                         "検索エンジン自作入門を買いました!!!")
-        self.assertEqual(normalize("アルゴリズム C"), "アルゴリズムC")
-        self.assertEqual(normalize("　　　ＰＲＭＬ　　副　読　本　　　"), "PRML副読本")
-        self.assertEqual(normalize("Coding the Matrix"), "Coding the Matrix")
-        self.assertEqual(normalize("南アルプスの　天然水　Ｓｐａｒｋｉｎｇ　Ｌｅｍｏｎ　レモン一絞り"),\
-                         "南アルプスの天然水Sparking Lemonレモン一絞り")
-        self.assertEqual(normalize("南アルプスの　天然水-　Ｓｐａｒｋｉｎｇ*　Ｌｅｍｏｎ+　レモン一絞り"),\
-                         "南アルプスの天然水- Sparking*Lemon+レモン一絞り")
-        self.assertEqual(normalize(u'ﾊﾟﾊﾟ'), u"パパ")
-        self.assertEqual(normalize(u'a˗֊‐‑‒–⁃⁻₋−'), "a-")
-        self.assertEqual(normalize(u'あ﹣－ｰ—―─━ー'), u"あー")
-        self.assertEqual(normalize(u'チルダ~∼∾〜〰～'), u"チルダ")
-        self.assertEqual(normalize('う゛ほﾟ'), "ゔぽ")
+        self.assertEqual(normalize('０'), '0')
+        self.assertEqual(normalize('ﾊﾝｶｸ'), 'ハンカク')
+        self.assertEqual(normalize('o₋o'), 'o-o')
+        self.assertEqual(normalize('majika━'), 'majikaー')
+        self.assertEqual(normalize('わ〰い'), 'わい')
+        self.assertEqual(normalize('スーパーーーー'), 'スーパー')
+        self.assertEqual(normalize('!#'), '!#')
+        self.assertEqual(normalize('ゼンカク　スペース'), 'ゼンカクスペース')
+        self.assertEqual(normalize('お             お'), 'おお')
+        self.assertEqual(normalize('      おお'), 'おお')
+        self.assertEqual(normalize('おお      '), 'おお')
+        self.assertEqual(normalize('検索 エンジン 自作 入門 を 買い ました!!!'),\
+                         '検索エンジン自作入門を買いました!!!')
+        self.assertEqual(normalize('アルゴリズム C'), 'アルゴリズムC')
+        self.assertEqual(normalize('　　　ＰＲＭＬ　　副　読　本　　　'), 'PRML副読本')
+        self.assertEqual(normalize('Coding the Matrix'), 'Coding the Matrix')
+        self.assertEqual(normalize('南アルプスの　天然水　Ｓｐａｒｋｉｎｇ　Ｌｅｍｏｎ　レモン一絞り'),\
+                         '南アルプスの天然水Sparking Lemonレモン一絞り')
+        self.assertEqual(normalize('南アルプスの　天然水-　Ｓｐａｒｋｉｎｇ*　Ｌｅｍｏｎ+　レモン一絞り'),\
+                         '南アルプスの天然水- Sparking*Lemon+レモン一絞り')
+        self.assertEqual(normalize('ﾊﾟﾊﾟ'), 'パパ')
+        self.assertEqual(normalize('a˗֊‐‑‒–⁃⁻₋−'), 'a-')
+        self.assertEqual(normalize('あ﹣－ｰ—―─━ー'), 'あー')
+        self.assertEqual(normalize('チルダ~∼∾〜〰～'), 'チルダ')
+        self.assertEqual(normalize('う゛ほﾟ'), 'ゔぽ')
 
     def test_normalize_lengthened(self):
-        self.assertEqual(normalize("うまああああああああああああい", repeat=7), "うまあああああああい")
-        self.assertEqual(normalize("かわいいいいいるい", repeat=6), "かわいいいいいるい")
+        self.assertEqual(normalize('うまああああああああああああい', repeat=7), 'うまあああああああい')
+        self.assertEqual(normalize('かわいいいいいるい', repeat=6), 'かわいいいいいるい')
 
 
 if __name__ == '__main__':
