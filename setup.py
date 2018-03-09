2d2e9a4ff0d17ceb19561cd458bcde68b7eb0bda
Ryuichi Yamamoto <zryuichi@gmail.com>
2018-03-09 15:25:08 +0900

    Add explicit mmacosx-version-min=10.7
    Without this complication fails with python 2.7.
    Hopefully this is ok

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ with open('neologdn.cpp', 'r', encoding='utf8') as f:
 
 extra_compile_args = ["-std=c++11"]
 if platform.system() == "Darwin":
+    extra_compile_args.append("-mmacosx-version-min=10.7")
     extra_compile_args.append("-stdlib=libc++")
 
 setup(
