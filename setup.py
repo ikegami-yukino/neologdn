419dbf0a810b0cd7c2bb4cbb70af37a1e3b0ca4c
Yukino Ikegami <yknikgm@gmail.com>
2021-05-02 10:32:41 +0900

    Drop supporting Python 3.5.X

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         'Programming Language :: Cython',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Text Processing :: Linguistic'
