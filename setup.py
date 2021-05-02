702f9dafca69948b6d622cd530ad3bae00984e1e
Yukino Ikegami <yknikgm@gmail.com>
2021-05-02 10:33:42 +0900

    Support Python 3.9

--- setup.py
+++ setup.py
@@ -33,6 +33,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Text Processing :: Linguistic'
     ],
     description='Japanese text normalizer for mecab-neologd',
